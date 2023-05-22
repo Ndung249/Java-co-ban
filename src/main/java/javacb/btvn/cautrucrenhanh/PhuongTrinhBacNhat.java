ed1cc0ba93f40507ad0cb1fb8b07845b495d6685
Ndung <96537438+TaiLe1101@users.noreply.github.com>
2023-05-22 16:17:44 +0700

    ptb1

--- src/main/java/javacb/btvn/cautrucrenhanh/PhuongTrinhBacNhat.java
+++ src/main/java/javacb/btvn/cautrucrenhanh/PhuongTrinhBacNhat.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package javacb.btvn.cautrucrenhanh;
+import java.util.Scanner;
 
 /**
  *
@@ -16,6 +18,20 @@ public class PhuongTrinhBacNhat {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new  Scanner(System.in);
+        double a, b, ketQua;
+        System.out.println("Hay nhap so A: ");
+        a=sc.nextDouble();
+        System.out.println("Hay nhap so B: ");
+        b=sc.nextDouble();
+        if (a==0) {
+            System.out.println("Phuong trinh vo nghiem ");    
+        }
+        else{
+        ketQua= -b/a;
+        System.out.println("Ket qua la : " + ketQua);
+        }
+        
     }
     
 }
